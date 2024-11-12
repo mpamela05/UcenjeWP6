f8ceac3dacf4d24e5e16ab40f8bb27e3f9f7cac1
mpamela05 <pamelamandic46@gmail.com>
2024-11-12 19:47:29 +0100

    Napravljen ostatak tablica baze zoo

--- zoo.sql
+++ zoo.sql
@@ -1,6 +1,6 @@
---create database zoo;
+create database zoo;
 
---use zoo;
+use zoo;
 
 create table zivotinja(
 sifra int,
@@ -9,4 +9,26 @@ ime varchar(50),
 djelatnik int,
 prostorija int,
 datum int
+);
+
+create table prostorija(
+sifra int,
+dimenzije varchar(30),
+maxsize int,
+mjesto varchar(30)
+);
+
+create table djelatnik(
+sifra int,
+ime varchar(5),
+prezime varchar(50),
+IBAN varchar(50)
+);
+
+
+create table datum(
+d_rodenja datetime,
+d_dolaska datetime,
+d_smrti datetime,
+sifra int
 );

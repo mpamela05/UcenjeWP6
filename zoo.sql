a2fa6e7d00763d8c6cfad5da2992c7d7eb8e6935
mpamela05 <pamelamandic46@gmail.com>
2024-11-13 18:04:26 +0100

    kreiranje tablica i dodavanje kljuceva zoo

--- zoo.sql
+++ zoo.sql
@@ -1,34 +1,45 @@
+use master;
+go
+-- brisem postojecu bazu ako postoji
+drop database if exists zoo;
+go
+
 create database zoo;
+go
 
 use zoo;
+go
 
-create table zivotinja(
-sifra int,
-vrsta varchar(50),
-ime varchar(50),
-djelatnik int,
-prostorija int,
-datum int
-);
+
+
 
 create table prostorija(
-sifra int,
-dimenzije varchar(30),
-maxsize int,
-mjesto varchar(30)
+sifra int not null primary key identity(1,1),
+dimenzije varchar(30) not null,
+maxsize int not null,
+mjesto varchar(30) not null
 );
 
 create table djelatnik(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
-IBAN varchar(50)
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+IBAN varchar(50) not null
 );
 
 
 create table datum(
-d_rodenja datetime,
-d_dolaska datetime,
+d_rodenja datetime not null,
+d_dolaska datetime not null,
 d_smrti datetime,
-sifra int
+sifra int not null primary key identity(1,1)
+);
+
+create table zivotinja(
+sifra int not null primary key identity(1,1),
+vrsta varchar(50) not null,
+ime varchar(50) not null,
+djelatnik int not null references djelatnik(sifra),
+prostorija int not null references prostorija(sifra),
+datum int not null references datum(sifra)
 );

acef33314b5b85a380e310b7477aebe5b8cce7f1
mpamela05 <pamelamandic46@gmail.com>
2024-11-16 15:56:48 +0100

    dodavanje informacija(tim,heroji)

--- marvel.sql
+++ marvel.sql
@@ -1,4 +1,4 @@
-use master;
+﻿use master;
 go
 
 drop database if exists marvel;
@@ -49,9 +49,65 @@ tim int references timovi(sifra)
 );
 
 insert into mjesto values ('Zemlja', 'New Zork'), ('Zemlja', 'Iowa'), ('Asgard', ''), ('Zemlja', 'Daiton'), ('Zemlja', 'Volgograd'), ('Zemlja', 'Hustville'), ('Zemlja', ''), ('Zemlja', 'St.Charles'), ('Planet_X', ''),
-('', 'Mount Kyles');
+('', 'Mount Kyles'), ('Zemlja', ''), ('The Benatar', '');
 select * from mjesto
 
-insert into identitet values ('Peter','Parker', '23', '2001', ''), ('Tony', 'Stark', '50', '1970', '2023'), ('Clint', 'Barton', '50', '1970',''), ('Thor', 'Odinson', '1052', '0965', ''), ('Natalia', 'Romanoff', '39', '1984', '2023'),
-(' Robert Bruce', 'Banner', '55', '1969', ''), ('Vision', '', '3', '2015', '2018'), ('Nicholas Joseph', 'Fury', '74', '1950',''), ('89P13', '', '','',''), ('Peter', 'Quill', '44', '1980', ''),
-('Groot', '', '10', '2014', ''), ('Drax', '', '', '', ''), ('Gamora', '', '', '', '2018');
+insert into identitet values ('Peter','Parker', '23', '2001', ''), ('Tony', 'Stark', '50', '1970', '2023'), ('Clint', 'Barton', '50', '1970',''), 
+('Thor', 'Odinson', '1052', '0965', ''), ('Natalia', 'Romanoff', '39', '1984', '2023'),
+(' Robert Bruce', 'Banner', '55', '1969', ''), ('Vision', '', '3', '2015', '2018'), ('Nicholas Joseph', 'Fury', '74', '1950',''), 
+('89P13', '', '','',''), ('Peter', 'Quill', '44', '1980', ''),
+('Groot', '', '10', '2014', ''), ('Drax', '', '', '', ''), ('Gamora', '', '', '', '2018'), ('Nebula', '', '', '', ''), ('Mantis', '', '', '', '');
+select * from identitet
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Peter Parker');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Tony Stark');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Clint Barton');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Asgard', 'Thor Odinson');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Rober Bruce Banner');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Vision');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Natalia Romanoff ');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Avengers', 'New York', 'Zemlja', 'Nicholas Joseph Fury');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', '89P13');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', 'Peter Quill');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', 'Groot');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', 'Drax');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', 'Gamora');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', 'Nebula');
+
+insert into timovi (naziv, baza, mjesto, identitet)
+values ('Guardians of the Galaxy', 'Andromeda galaxy', '', 'Mantis');
+select * from timovi
+
+insert into heroji (ime, moc, mjesto, osobnost, g_dolaska, identitet, tim)
+values ('Spider Man', 'nadljudska snaga, izdržljivost, brzina, regeneracija, mogućnost ispaljivanja paukovih mreža, penjanje po raznim površinama',
+'New York', 'hrabar,brizan,lojalan,drag', '2016', 'Peter Parker', 'Avengers');
+
+insert into heroji (ime, moc, mjesto, osobnost, g_dolaska, identitet, tim)
+values ('Iron Man', 'mocno oklopno odijelo, projektili, mogucnost letenja', 'New York', 'hrabar, inteligentan, ponosan, ekscentirican', '2007', 'Tony Stark', 'Avengers');

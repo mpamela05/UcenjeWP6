907f00a74ddfd58efd8f034c511073e6577dbea4
mpamela05 <pamelamandic46@gmail.com>
2024-11-27 17:47:12 +0100

    godine promijenjene u int i maknuti navodnici sa null

--- aplikacija/marvel.sql
+++ aplikacija/marvel.sql
@@ -13,10 +13,10 @@ go
 create table identitet(
 sifra int not null primary key identity(1,1),
 ime varchar(50) not null,
-prezime varchar(50)  not null,
-godine int not null,
-god_rodjenja date not null,
-god_smrti date null
+prezime varchar(50) null,
+godine int null,
+god_rodjenja int null,
+god_smrti int null
 );
 
 
@@ -33,27 +33,27 @@ ime varchar(50) not null,
 moc varchar(250) not null,
 mjesto varchar(100),
 osobnost varchar(100) not null,
-g_dolaska date not null,
+g_dolaska int not null,
 identitet int references identitet(sifra),
 tim int references timovi(sifra)
 );
 
 insert into identitet
-values('Peter','Parker', '23', '2001', 'null'), 
-('Tony', 'Stark', '50', '1970', '2023'), 
-('Clint', 'Barton', '50', '1970','null'), 
-('Thor', 'Odinson', '1052', '0965', 'null'), 
-('Natalia', 'Romanoff', '39', '1984', '2023'),
-(' Robert Bruce', 'Banner', '55', '1969', 'null'), 
-('Vision', 'null', '3', '2015', '2018'), 
-('Nicholas Joseph', 'Fury', '74', '1950','null'), 
-('89P13', 'null', 'null','null','null'), 
-('Peter', 'Quill', '44', '1980', 'null'),
-('Groot', 'null', '10', '2014', 'null'), 
-('Drax', 'null', 'null', 'null', 'null'), 
-('Gamora', 'null', 'null', 'null', '2018'), 
-('Nebula', 'null', 'null', 'null', 'null'), 
-('Mantis', 'null', 'null', 'null', 'null');
+values('Peter','Parker', 23, 2001, null), 
+('Tony', 'Stark', 50, 1970, 2023), 
+('Clint', 'Barton', 50, 1970,null), 
+('Thor', 'Odinson', 1052, 0965, null), 
+('Natalia', 'Romanoff', 39, 1984, 2023),
+(' Robert Bruce', 'Banner', 55, 1969, null), 
+('Vision', 'null', 3, 2015, 2018), 
+('Nicholas Joseph', 'Fury', 74, 1950,null), 
+('89P13', null, 5, null, null), 
+('Peter', 'Quill', 44, 1980, null),
+('Groot', null, 10, 2014, null), 
+('Drax', null, null, null, null), 
+('Gamora', null, null, null, 2018), 
+('Nebula', null, null, null, null), 
+('Mantis', null, null, null, null);
 select * from identitet
 
 insert into timovi (naziv, mjesto)
@@ -62,7 +62,7 @@ select * from timovi
 
 insert into heroji (ime, moc, mjesto, osobnost, g_dolaska, identitet, tim)
 values ('Spider Man', 'nadljudska snaga, izdržljivost, brzina, regeneracija, mogućnost ispaljivanja paukovih mreža, penjanje po raznim površinama',
-'New York', 'hrabar,brizan,lojalan,drag', '2016', 1, 1);
+'New York', 'hrabar,brizan,lojalan,drag', 2016, 1, 1);
 
 insert into heroji (ime, moc, mjesto, osobnost, g_dolaska, identitet, tim)
 values ('Iron Man', 'mocno oklopno odijelo, projektili, mogucnost letenja', 'New York', 'hrabar, inteligentan, ponosan, ekscentirican', 2007, 2, 1);

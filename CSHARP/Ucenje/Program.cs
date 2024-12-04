38c787329e3dbbb012404244b2d27b95224a407a
mpamela05 <pamelamandic46@gmail.com>
2024-12-04 15:58:34 +0100

    Ucenje projekt

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav svijetu ČĆŠŽĐ čćšžđ");
-Console.WriteLine("Hello Osijek");
-
-
-
-
-
+Console.WriteLine("Hello, World!");

4c89e012f4e8a438666bf7a3e5a42ac3efe0df27
mpamela05 <pamelamandic46@gmail.com>
2024-12-04 17:01:20 +0100

    vjezba sa sata

---
+++ CSHARP/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+
+        public static void Izvedi() 
+        {
+
+            Console.WriteLine("Unesi svoje ime: ");
+
+            string Ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste" + Ime);
+
+            Console.WriteLine("Unijeli ste {0}. Bravo!", Ime)
+        }
+
+
+    }
+}
--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,11 +1,17 @@
-﻿// komentar jedna linija
+﻿using Ucenje;
+
+E01UlazIzlaz.Izvedi();
+
+// komentar jedna linija
 
 /*
+
+
 
 komentar vise linija
 
 */
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav svijetu ČĆŽŠĐ čćžšđ");
-Console.WriteLine("Hello Osijek");
+//Console.WriteLine("Pozdrav svijetu ČĆŽŠĐ čćžšđ");
+//Console.WriteLine("Hello Osijek");

79991d6a72194a9622b5fb02f667450172b5d941
mpamela05 <pamelamandic46@gmail.com>
2024-12-06 17:51:37 +0100

    provjera godina zadatak

---
+++ CSHARP/Ucenje/E04Z1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E04Z1
+    {
+        public static void Izvedi() {
+            int godine;
+            Console.WriteLine("unesite broj godina");
+            godine=int.Parse(Console.ReadLine());
+            if (godine < 1 | godine >= 122) {
+                Console.WriteLine("greska");
+            }
+            else {
+                if (godine > 17)
+                {
+                    Console.WriteLine("osoba je punoljetna");
+                }
+                else {
+                    Console.WriteLine("osoba je maloljetna");
+
+                }
+            }
+        }
+    }
+}
--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Ucenje;
-E04UvijetnoGrananjeIf.Izvedi();
+E04Z1.Izvedi();
+//E04UvijetnoGrananjeIf.Izvedi();
 //E03Z1.Izvedi(); 
 //E03Operatori.Izvedi();
 //E02Z1.Izvedi();

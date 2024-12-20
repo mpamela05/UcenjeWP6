549dd1627be58e287c74bf2c89cba652d2218821
mpamela05 <pamelamandic46@gmail.com>
2024-12-20 18:54:40 +0100

    Vjezba: parnost broja, tablica, jedincna vrijednost, broj znakova mjesta

--- CSHARP/Ucenje/E14Vjezbanje.cs
+++ CSHARP/Ucenje/E14Vjezbanje.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,9 @@ namespace Ucenje
 {
     internal class E14Vjezbanje
     {
+        //3.program: za dani cijeli broj izmedju 20 i 50 ispisi koja mu je vrijednost jeinice
+        //34 treba ispisat 4
+        // 21 treba ispisat 1
         public static void Izvedi()
         {
             Console.WriteLine("Dobro dosli u vjezbanje petkom");
@@ -18,17 +23,28 @@ namespace Ucenje
 
         private static void Izbornik()
         {
+
+            string[] programi =
+            {
+                "Parnost broja",
+                "Tablica mnozenja",
+                "Jedinicna vrijednost",
+                "Broj znakoa naiva mjesta"
+            };
+
             Console.WriteLine();
             Console.WriteLine("IZBORNIK");
-            Console.WriteLine("1. Parnost broja");
-            Console.WriteLine("2. Tablica mnozenja");
+            for(int i = 0;i < programi.Length; i++)
+            {
+                Console.WriteLine("{0}. {1}", i+1,  programi[i]);
+            }
             Console.WriteLine("0. Izlaz iz programa");
-            OdabirOpcijeIzbornika();
+            OdabirOpcijeIzbornika(programi.Length);
         }
 
-        private static void OdabirOpcijeIzbornika()
+        private static void OdabirOpcijeIzbornika(int brojPrograma)
         {
-            switch(E12Metode.UcitajCijeliBroj("Odaberi stavku izbornika: ",0,2))
+            switch (E12Metode.UcitajCijeliBroj("Odaberi stavku izbornika: ", 0,brojPrograma))
             {
                 case 0:
                     break;
@@ -39,27 +55,54 @@ namespace Ucenje
                 case 2:
                     TablicaMnozenja();
                     Izbornik();
-                    break; 
-                      
-               
+                    break;
+                case 3:
+                    JedinicnaVrijednost();
+                    Izbornik();
+                    break;
+                case 4:
+                    BrojZnakovaNaziaMjesta();
+                    Izbornik();
+                    break;
+
+
+
             }
+        }
+
+        private static void BrojZnakovaNaziaMjesta()
+        {
+            NaslovPrograma("Za uneseni naziv mjesta ispisuje koliko ima znaova");
+            Console.WriteLine("Unesite naziv mjesta: ");
+            string ime = E12Metode.UcitajString("Unesi ime grada: ");
+            Console.WriteLine(ime.Length);
         }
 
         private static void TablicaMnozenja()
         {
-            
+
             NaslovPrograma("program koji za dane brojeve redka i tupaca generira tablicu mnozennja");
-           
+            int redaka = E12Metode.UcitajCijeliBroj("unesi broj redaka: ", 2, 10);
+            int stupaca = E12Metode.UcitajCijeliBroj("unesi broj stupaca: ", 2, 10);
+
+            for (int i = 1; i <= redaka; i++)
+            {
+                for (int j = 1; j <= stupaca; j++)
+                {
+                    Console.Write("{0,4}", i * j);
+                }
+                Console.WriteLine();
+            }
 
         }
 
         private static void ParnostBroja()
         {
-            Console.WriteLine("***************");
+
             Console.WriteLine("program koji provjerava da li je uneseni broj paran ili ne");
-            Console.WriteLine("*****************");
+
             int broj = E12Metode.UcitajCijeliBroj("Unesi cijeli broj: ");
-            if (broj%2 == 0) 
+            if (broj % 2 == 0)
             {
                 Console.WriteLine("uneseni broj {0} je PARAN", broj);
             }
@@ -72,20 +115,35 @@ namespace Ucenje
         }
         private static void NaslovPrograma(string naslov)
         {
-            
-            Zvjezdice(naslov.Length);
+
+            NaglasiNaslov(naslov.Length);
             Console.WriteLine(naslov);
-            Zvjezdice(naslov.Length);
+            NaglasiNaslov(naslov.Length);
 
         }
-        private static void Zvjezdice(int komada)
+        private static void NaglasiNaslov(int komada)
         {
-            for (int i = 0; i <komada; i++)
+            for (int i = 0; i < komada; i++)
             {
                 Console.Write("-");
             }
             Console.WriteLine();
 
+        }
+        private static void JedinicnaVrijednost()
+        {
+            NaslovPrograma("JedinicnaVrijdnost");
+            Console.WriteLine("unesite cijeli broj izmedju 20 i 50");
+            int broj = int.Parse(Console.ReadLine());
+            if (broj >= 20 && broj <= 50)
+            {
+                int jedinica = broj % 10;
+                Console.WriteLine("vrijednost jedinica broja {0} je {1}", broj,jedinica);
+            } 
+            else
+            {
+                Console.WriteLine("Broj mora biti izmedju 20 i 50");
+            }
         }
     }
 }
